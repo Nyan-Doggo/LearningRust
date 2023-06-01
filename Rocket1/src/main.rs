35446f5d7596ce27cb929c3fb625177109e9f46f
Nyan <70441087+Nyan-Doggo@users.noreply.github.com>
2023-06-02 00:14:03 +0200

    minor testing in rocket

--- Rocket1/src/main.rs
+++ Rocket1/src/main.rs
@@ -1,14 +1,20 @@
-// i have no clue what this line menas
-#[macro_use] extern crate rocket;
+// import the stuff we need from rocket
+use rocket::*;
 
 // defines how to handle a get-request at the route "/"
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "hello world"
+}
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    tokio::time::sleep(tokio::time::Duration::from_secs(seconds)).await;
+    format!("slept for {seconds} seconds")
 }
 
 // starts the server
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, delay])
 }
