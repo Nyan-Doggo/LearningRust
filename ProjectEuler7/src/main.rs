3ef33038c6ab8b1c70e032af5953c84f0bea8a24
Nyan <70441087+Nyan-Doggo@users.noreply.github.com>
2023-06-02 10:50:43 +0200

    clarified solution for p7

--- ProjectEuler7/src/main.rs
+++ ProjectEuler7/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     let mut primes: Vec<i32> = vec![2, 3];
 
+    // check 6n-1 and 6n+1, start with an offset of -1,
+    // so basically stat at 5, step by 6, (5, 11, 17)
+    // and check for n and n+2 (5 and 7, 11 and 13, 17 and 19)
     let mut a: i32 = 5;
     while primes.len() < 10001 {
         if is_prime(a) {
@@ -16,6 +19,7 @@ fn main() {
 }
 
 fn is_prime(n: i32) -> bool {
+    // basic conditions
     if n < 2 {
         return false;
     } else if n == 2 || n == 3 {
@@ -25,7 +29,8 @@ fn is_prime(n: i32) -> bool {
     }
 
     let mut test: i32 = 3;
-
+    // assumption: if not divisible by anything below sqrt()
+    // then not divisible at all, hence prime
     while test < ((n as f32).sqrt() + 1.0) as i32 {
         if n % test == 0 {
             return false;
@@ -33,5 +38,6 @@ fn is_prime(n: i32) -> bool {
         test += 2;
     }
 
+    // passed all the tests, must be prime
     return true;
 }
