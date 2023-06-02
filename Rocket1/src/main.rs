54beda98c6cc14f226d6550c0b2ed37bda812b57
Nyan <70441087+Nyan-Doggo@users.noreply.github.com>
2023-06-02 11:40:21 +0200

    looked a bit more at rocket ^¨^

--- Rocket1/src/main.rs
+++ Rocket1/src/main.rs
@@ -16,5 +16,8 @@ async fn delay(seconds: u64) -> String {
 // starts the server
 #[launch]
 fn rocket() -> _ {
+    // mounting was hard to get, but it seems like this gets the "decorator" route and appends it to the
+    // root at "/", so you could say that "mount x [fny, fnz, fnw]" prepeds X to the path for all the functions
+    // in a sense? maybe maybe. this works tho ^~^
     rocket::build().mount("/", routes![index, delay])
 }
